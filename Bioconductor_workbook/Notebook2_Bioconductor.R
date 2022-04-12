06bf11097b21f7f99ba4b2e8e4c70c6198ef8871
isobarkha <barkhahahaha@gmail.com>
2022-04-12 18:21:31 +0530

    updated notebook

--- Bioconductor_workbook/Notebook2_Bioconductor.R
+++ Bioconductor_workbook/Notebook2_Bioconductor.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #-------------------------------------------------------------------------------------------
 ## GenomicRanges: Basic Usage
 #-------------------------------------------------------------------------------------------
@@ -32,47 +31,11 @@ seqinfo(gr) #Not much. Let's fill more info and check later. Note that 'my chrom
 ##--- ---
 ##--- ---
 ##--- ---
-=======
-#-------------------------------------------------------------------------------------------
-## GenomicRanges: Basic Usage
-#-------------------------------------------------------------------------------------------
-
-
-##-----Installing or loading GenomicRanges package-----
-if (!require("BiocManager"))
-  install.packages("BiocManager")
-BiocManager::install("GenomicRanges")
-
-#Load the GenomicRanges Package
-library(GenomicRanges)
-
-##-----Creating a GRange----- using GRanges(). 
-gr = GRanges(seqnames = c("chr1"), strand = c("+", "-", "+"), ranges = IRanges(start = c(1,3,5), width = 3)) #chromosome name = seqnames, strand = DNA Strand identity
-gr #returns 3 sequences for two different strands of the same chromosome
-#chromosome strands can be of 3 types: "+", "-", "*" <* indicates unknown strand/entity present on both strand irrespective of direction> 
-
-##-----What is the flanking sequence on the Grange?-----  
-flank(gr, 5) #to get a GRanges object containing the ranges that include the 5 bases upstream of the ranges
-#region that flanks would be relative to direction of transcription (explains the + and negative values returned to tell if the region is flanking from the left or the right side of my GRange)
-
-##-----What would be the Promoter on this range------
-promoters(gr)
-#this gives a 2200 base pair interval where 200 bases are downstream and 2000 bases are upstream of the transcription start site
-
-##-----What is known of my chromosome of interest-----
-seqinfo(gr) #Not much. Let's fill more info and check later. Note that 'my chromosome' = chromosome of interest being investigated at all times
-
-##-----Adding information about my chromosome-----
-##--- ---
-##--- ---
-##--- ---
-
 ##---Define Length---
 seqlengths(gr) = c("chr1" = 10)
 seqinfo(gr)
 seqlevels(gr)
 #since a length end of chromosome is also defined now, we can talk of attribtes that are relative to chromosome as a whole
-
 
 #--- Adding more chromosomes---
 seqnames(gr) = c("chr1", "chr2", "chr1") #gives an error because it has recorded that this hypothetical organism only has a single chromosome <Error in .normalize_seqnames_replacement_value(value, x) : levels of supplied 'seqnames' must be identical to 'seqlevels(x)'>
@@ -83,6 +46,10 @@ gr
 
 #--- Is my genome circular ---
 isCircular(gr) = c(FALSE, TRUE) 
+seqinfo(gr)
+
+#--- Assigning a genome to this GRange ---
+genome(gr) = "hg19" #nice info label to have on our genome since sequence/DNA might even be coming from different organisms or like a control/test sitation later. Protects from any errors later
 seqinfo(gr)
 
 #---Add a new genome---
@@ -99,3 +66,5 @@ sort(gr)
 ##-----Gaps on my chromosome-----
 #Gaps is a function that will give all the area on chromosome that is not covered by ranges in the GRanges
 gaps(gr)
+
+
